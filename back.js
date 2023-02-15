9acf9d9711af10f6413b1fb544aa4f1e92d3294a
GuillaumeBdrd <guillaume.bodard@orange.fr>
2023-02-15 15:07:10 +0100

    git commit

---
+++ back.js
@@ -0,0 +1,6 @@
+function calculer(){
+    var nb1 = document.getElementById("nb1").value;
+    var nb2 = document.getElementById("nb2").value;
+    document.getElementById("result").innerHTML=nb1*nb2;
+
+}

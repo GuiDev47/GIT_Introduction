e3e0a3f7575e89ec512101da543f2d3c202f3a0f
GuillaumeBdrd <guillaume.bodard@orange.fr>
2023-02-28 10:37:43 +0100

    commit principal

--- back.js
+++ back.js
@@ -1,6 +1,18 @@
-function calculer(){
-    var nb1 = document.getElementById("nb1").value;
-    var nb2 = document.getElementById("nb2").value;
-    document.getElementById("result").innerHTML=nb1*nb2;
+let prixEntrées =[10,12,13];
+let prixPlats = [11,12,13];
+let prixDesserts = [9,15,7];
+
+function calculerPrix(){
+    var prix1  = prixEntrées[document.getElementsByClassName("select")[0].selectedIndex];
+    document.getElementById("prixEntrée").innerHTML = prix1 + " €";
+
+    var prix2  = prixPlats[document.getElementsByClassName("select")[1].selectedIndex];
+    document.getElementById("prixPlat").innerHTML = prix2 + " €";
+    
+    var prix3  = prixDesserts[document.getElementsByClassName("select")[2].selectedIndex];
+    document.getElementById("prixDessert").innerHTML = prix3 + " €";
+
+    var prixTotal = prix1 + prix2 + prix3;
+    document.getElementById("Total").innerHTML = prixTotal + " €";
 
 }
